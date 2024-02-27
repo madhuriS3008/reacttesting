4865ef6518fed87598c0736ab9e18674cda80efa
Madhuri Sabbineni <madhusabbineni@gmail.com>
2024-02-26 21:04:57 -0600

    initial test case for practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <a href="https://www.google.com/imgres?imgurl=https%3A%2F%2Fbuffer.com%2Flibrary%2Fcontent%2Fimages%2Fsize%2Fw1200%2F2023%2F10%2Ffree-images.jpg&tbnid=ivTDs79HInLVcM&vet=12ahUKEwi19N6p7cmEAxUO2ckDHSumBG0QMygBegQIARBM..i&imgrefurl=https%3A%2F%2Fbuffer.com%2Flibrary%2Ffree-images%2F&docid=U9G_8UXPMlqatM&w=1200&h=800&itg=1&q=image&ved=2ahUKEwi19N6p7cmEAxUO2ckDHSumBG0QMygBegQIARBM">
+        Learn React
+      </a>
+    </div>
+  );
 }
 
 export default App;
